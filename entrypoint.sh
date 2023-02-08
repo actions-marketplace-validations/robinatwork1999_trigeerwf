e7cee9f4af98028561fc74ca7a6e7cbf3164d685
robinatwork1999 <103563730+robinatwork1999@users.noreply.github.com>
2023-02-08 19:06:02 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -175,7 +175,7 @@ wait_for_workflow_to_finish() {
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${INPUT_COMMENT_GITHUB_TOKEN}"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/robinatwork1999/product-commerce/actions/runs/4124459160/logs)
+  https://api.github.com/repos/robinatwork1999/product-commerce/actions/runs/${last_workflow_id}/logs)
   then
     echo "response=$response"
   else
