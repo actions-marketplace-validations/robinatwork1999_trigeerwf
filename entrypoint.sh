81428b6ef91cac1035726dd3d0fa2b50a718cb28
robinatwork1999 <103563730+robinatwork1999@users.noreply.github.com>
2023-02-09 00:13:26 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,48 +4,28 @@ set -e
 GITHUB_API_URL="${API_URL:-https://api.github.com}"
 GITHUB_SERVER_URL="${SERVER_URL:-https://github.com}"
 
-validate_args() {
-  wait_interval=10 # Waits for 10 seconds
-  if [ "${INPUT_WAIT_INTERVAL}" ]
-  then
-    wait_interval=${INPUT_WAIT_INTERVAL}
-  fi
+validateArgs() {
+  # Setup flags
+  triggerWorkflow=true
+  waitWorkflow=true
+  ref="main"
 
-  propagate_failure=true
-  if [ -n "${INPUT_PROPAGATE_FAILURE}" ]
+  # Necessary I/P Checks
+  if [ -z "${INPUT_ORG}" ]
   then
-    propagate_failure=${INPUT_PROPAGATE_FAILURE}
-  fi
-
-  trigger_workflow=true
-  if [ -n "${INPUT_TRIGGER_WORKFLOW}" ]
-  then
-    trigger_workflow=${INPUT_TRIGGER_WORKFLOW}
-  fi
-
-  wait_workflow=true
-  if [ -n "${INPUT_WAIT_WORKFLOW}" ]
-  then
-    wait_workflow=${INPUT_WAIT_WORKFLOW}
-  fi
-
-  if [ -z "${INPUT_OWNER}" ]
-  then
-    echo "Error: Owner is a required argument."
+    echo "Error: ORG is a required argument."
     exit 1
   fi
 
-  if [ -z "${INPUT_REPO}" ]
+  if [ -z "${INPUT_REPOSITORY}" ]
   then
-    echo "Error: Repo is a required argument."
+    echo "Error: Repository is a required argument."
     exit 1
   fi
 
-  if [ -z "${INPUT_GITHUB_TOKEN}" ]
+  if [ -z "${INPUT_REPO_TOKEN}" ]
   then
-    echo "Error: Github token is required. You can head over settings and"
-    echo "under developer, you can create a personal access tokens. The"
-    echo "token requires repo access."
+    echo "Error: Repository token is required."
     exit 1
   fi
 
@@ -55,16 +35,12 @@ validate_args() {
     exit 1
   fi
 
-  client_payload=$(echo '{}' | jq -c)
-  if [ "${INPUT_CLIENT_PAYLOAD}" ]
+  if [ -z "${INPUT_CLIENT_PAYLOAD}" ]
   then
-    client_payload=$(echo "${INPUT_CLIENT_PAYLOAD}" | jq -c)
-  fi
-
-  ref="main"
-  if [ "$INPUT_REF" ]
-  then
-    ref="${INPUT_REF}"
+    echo "Error: Brandname is required"
+    exit 1
+  else 
+    clientPayload=$(echo "${INPUT_CLIENT_PAYLOAD}" | jq -c)  
   fi
 }
 
@@ -91,25 +67,24 @@ api() {
 }
 
 
-# Return the ids of the most recent workflow runs, optionally filtered by user
-get_workflow_runs() {
+getWorkflowData() {
   since=${1:?}
   query="event=workflow_dispatch&created=>=$since${INPUT_GITHUB_USER+&actor=}${INPUT_GITHUB_USER}&per_page=100"
   api "workflows/${INPUT_WORKFLOW_FILE_NAME}/runs?${query}" |
   jq -r '.workflow_runs[].id' |
-  sort # Sort to ensure repeatable order, and lexicographically for compatibility with join
+  sort
 }
 
-trigger_workflow() {
+triggerWorkflowHandler() {
   START_TIME=$(date +%s)
-  SINCE=$(date -u -Iseconds -d "@$((START_TIME - 120))") # Two minutes ago, to overcome clock skew
+  SINCE=$(date -u -Iseconds -d "@$((START_TIME - 120))")
 
-  OLD_RUNS=$(get_workflow_runs "$SINCE")
+  OLD_RUNS=$(getWorkflowData "$SINCE")
 
-  echo >&2 "Triggering workflow:"
+  echo >&2 "Triggering Workflow For Syncing Platform"
 
   api "workflows/${INPUT_WORKFLOW_FILE_NAME}/dispatches" \
-    --data "{\"ref\":\"${ref}\",\"inputs\":${client_payload}}"
+    --data "{\"ref\":\"${ref}\",\"inputs\":${clientPayload}}"
 
   NEW_RUNS=$OLD_RUNS
   while [ "$NEW_RUNS" = "$OLD_RUNS" ]
@@ -121,35 +96,12 @@ trigger_workflow() {
   join -v2 <(echo "$OLD_RUNS") <(echo "$NEW_RUNS")
 }
 
-
-comment_downstream_link() {
-  if response=$(curl --fail-with-body -sSL -X POST \
-      "${INPUT_COMMENT_DOWNSTREAM_URL}" \
-      -H "Authorization: Bearer ${INPUT_COMMENT_GITHUB_TOKEN}" \
-      -H 'Accept: application/vnd.github.v3+json' \
-      -d "{\"body\": \"Running downstream job at $1\"}")
-  then
-    echo "$response"
-  else
-    echo >&2 "failed to comment to ${INPUT_COMMENT_DOWNSTREAM_URL}:"
-  fi
-}
-
-wait_for_workflow_to_finish() {
+workflowStallHandler() {
   last_workflow_id=${1:?}
   last_workflow_url="${GITHUB_SERVER_URL}/${INPUT_OWNER}/${INPUT_REPO}/actions/runs/${last_workflow_id}"
 
 
-  echo "Waiting for workflow to finish:"
-  echo "The workflow id is [${last_workflow_id}]."
-  echo "The workflow logs can be found at ${last_workflow_url}"
-  echo "workflow_id=${last_workflow_id}" >> $GITHUB_OUTPUT
-  echo "workflow_url=${last_workflow_url}" >> $GITHUB_OUTPUT
-  echo ""
-
-  if [ -n "${INPUT_COMMENT_DOWNSTREAM_URL}" ]; then
-    comment_downstream_link ${last_workflow_url}
-  fi
+  echo "Syncing the Platform Changes...:"
 
   conclusion=null
   status=
@@ -163,8 +115,8 @@ wait_for_workflow_to_finish() {
 
   if [[ "${conclusion}" == "success" && "${status}" == "completed" ]]
   then
-    echo "Workflow Completed Successfully"
-    echo "Fetching The PR Link"
+    echo "Platform Synced Successfully..."
+    echo "Fetching The PR Link..."
 
     if response=$(curl -sSL \
     -H "Accept: application/vnd.github+json" \
@@ -174,7 +126,7 @@ wait_for_workflow_to_finish() {
     then
     if [[ ! -n "$response" ]]
      then 
-     echo $response
+     echo "PR LINK": $response
      else 
      echo "No PR Found"
      fi 
@@ -182,35 +134,21 @@ wait_for_workflow_to_finish() {
     echo "PR Link Not Fetched Due To Some Error"
     fi
   else
-    echo "Conclusion is not success, it's [${conclusion}]."
-
-    if [ "${propagate_failure}" = true ]
-    then
-      echo "Propagating failure to upstream job"
+    echo "Platform syncing failed due to some error"
       exit 1
     fi
   fi
 }
 
-main() {
-  validate_args
+entrypoint() {
+  validateArgs
 
-  if [ "${trigger_workflow}" = true ]
-  then
-    run_ids=$(trigger_workflow)
-  else
-    echo "Skipping triggering the workflow."
-  fi
+    jobIds=$(triggerWorkflowHandler)
 
-  if [ "${wait_workflow}" = true ]
-  then
-    for run_id in $run_ids
+    for jobId in $jobIds
     do
-      wait_for_workflow_to_finish "$run_id"
+      workflowStallHandler "$jobId"
     done
-  else
-    echo "Skipping waiting for workflow."
-  fi
 }
 
-main
+entrypoint
